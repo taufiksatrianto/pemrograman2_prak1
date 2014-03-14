f3e3cf511c0475a3d34222d6e82caad2b945c09c
hanz <hanz.locker22@gmail.com>
2014-03-14 15:36:55 +0700

    pemrograman2_prak1

---
+++ praktikum2-1/src/main/java/latihan/praktikum2/App.java
@@ -0,0 +1,21 @@
+package latihan.praktikum2;
+
+/**
+ * Hello world!
+ *
+ */
+ 
+ import org.joda.time.DateTime;
+ 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		
+		DateTime dt = new DateTime();
+		System.out.println("waktu ini : " + dt);
+    }
+	
+}
+	
